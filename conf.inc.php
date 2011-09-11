772c8f7ee15fa48cfb49a8f8a72664028a8808ed
Gilles MARCHAULT <gmarchault@yahoo.com>
2011-09-11 22:27:43 +0200

    test depuis pc2

---
+++ conf.inc.php
@@ -0,0 +1,37 @@
+<?php 
+require_once "bdd_dev.inc.php";
+
+$PREFIX_BDD = "rel_";
+
+$NAVIGATEUR_MOBILE = "Mobile";
+$NAVIGATEUR_PC = "PC";
+
+$PATH_WEB_RELATIF = "/myhome";
+$PATH_WEB_ABSOLU = "http://$WEB$PATH_WEB_RELATIF";
+
+// Rep a purger :
+$PHP_SELF = $_SERVER['PHP_SELF'];
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+$UPLOAD_REP = "$DOCUMENT_ROOT/tmp";
+
+//$GRAPHE_FORMAT = "gif";
+$GRAPHE_FORMAT = "jpg";
+
+$GRAPHE_REP_WEB = "$PATH_WEB_RELATIF/releve/graphe/bitmap";
+$GRAPHE_REP_DISK = "$DOCUMENT_ROOT/$GRAPHE_REP_WEB";
+$GRAPHE_NOM_UNIQUE = 0; // Pour eviter les pbs des images cachees.
+
+$XML_PATH_CONFFILE = "$DOCUMENT_ROOT/$PATH_WEB_RELATIF/releve/graphe/generation/bitmap/xml";
+
+$MODE_DEBUG = 1;
+
+require_once "utilitaire.inc.php";
+require_once "cerreur.inc.php";
+require_once "cconnexion.inc.php";
+require_once "template_ihm.inc.php";
+
+// L'ordre est important :
+$ihm = new CIhm();
+$erreur = new CErreur();
+$cnx = new CConnexion( $DATABASE );
+?>
